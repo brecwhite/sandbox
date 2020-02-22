efb279c2daa3d4ef8123b83b7570c65152a90537
brecwhite <59800248+brecwhite@users.noreply.github.com>
2020-02-22 15:50:08 -0500

    array practice

---
+++ arrayP.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int arr[10];
+	
+	cin >> arr[0];
+	cout << arr[0];
+	
+	cin >> arr[1];
+	cout << arr[1];
+	
+	cin >> arr[2];
+	cout << arr[2];
+	
+	cin >> arr[3];
+	cout << arr[3];
+	
+	cin >> arr[4];
+	cout << arr[4];
+	
+	cin >> arr[5];
+	cout << arr[5];
+	
+	cin >> arr[6];
+	cout << arr[6];
+	
+	cin >> arr[7];
+	cout << arr[7];
+	
+	cin >> arr[8];
+	cout << arr[8];
+	
+	cin >> arr[9];
+	cout << arr [9];	
+	
+	return 0;
+}
